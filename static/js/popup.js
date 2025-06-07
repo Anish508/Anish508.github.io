b1a73f0da7427f76df3de6974e0a714c0fb5fb89
ShrinidhiM13 <shrinidhim9168@gmail.com>
2025-06-07 12:35:38 +0530

    new file:   .gitignore

---
+++ static/js/popup.js
@@ -0,0 +1,36 @@
+function openModal1() {
+  document.getElementById("popupModal1").classList.add("active");
+}
+function closeModal1() {
+  document.getElementById("popupModal1").classList.remove("active");
+}
+function openModal2() {
+  document.getElementById("popupModal2").classList.add("active");
+}
+function closeModal2() {
+  document.getElementById("popupModal2").classList.remove("active");
+}
+function openModal3() {
+  document.getElementById("popupModal3").classList.add("active");
+}
+function closeModal3() {
+  document.getElementById("popupModal3").classList.remove("active");
+}
+function openModal4() {
+  document.getElementById("popupModal4").classList.add("active");
+}
+function closeModal4() {
+  document.getElementById("popupModal4").classList.remove("active");
+}
+function openModal5() {
+  document.getElementById("popupModal5").classList.add("active");
+}
+function closeModal5() {
+  document.getElementById("popupModal5").classList.remove("active");
+}
+function openModal6() {
+  document.getElementById("popupModal6").classList.add("active");
+}
+function closeModal6() {
+  document.getElementById("popupModal6").classList.remove("active");
+}
